111ef6d1a2609ed3827229897e2ae051dd5b4e81
yutaka.miyazaki <mono.12dev@gmail.com>
2021-03-10 23:48:29 +0900

    chore: initial setting

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+	root: true,
+	env: {
+		browser: true,
+		es6: true,
+		jest: true,
+		node: true,
+	},
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 7,
+		sourceType: 'module',
+		ecmaFeatures: {
+			jsx: true,
+			modules: true,
+		},
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:@typescript-eslint/eslint-recommended',
+		'prettier',
+		'prettier/@typescript-eslint',
+	],
+	plugins: ['@typescript-eslint', 'prettier', 'react'],
+	rules: {
+		'react/react-in-jsx-scope': 'off',
+		'react/prop-types': 'off',
+		'react/no-unescaped-entities': 'off',
+		'@typescript-eslint/explicit-module-boundary-types': 'off',
+		'@typescript-eslint/no-empty-function': 'off',
+		'@typescript-eslint/no-explicit-any': 'off',
+		'@typescript-eslint/no-redeclare': 'off',
+		'@typescript-eslint/no-namespace': 'warn',
+		'@typescript-eslint/no-require-imports': 'warn',
+		'@typescript-eslint/no-unused-vars': 'warn',
+		'@typescript-eslint/prefer-for-of': 'warn',
+	},
+	settings: {
+		react: {
+			version: 'detect', // detect the version of React to use
+		},
+	},
+}
