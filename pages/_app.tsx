28418200b4439a2d2452fbe9167017a53cb0ea28
yutaka.miyazaki <mono.12dev@gmail.com>
2021-04-29 21:48:52 +0900

    update lint, script, global style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import { AppProps } from 'next/app'
 import 'sanitize.css'
 import 'sanitize.css/forms.css'
-import 'styles/global.css'
+import 'styles/global.scss'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
 	return <Component {...pageProps} />
