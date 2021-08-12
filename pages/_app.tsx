5161207aa849cdf05c23736b6ca603a19cbf8933
yutaka.miyazaki <mono.12dev@gmail.com>
2021-08-12 16:42:36 +0900

    feat: add next-seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,18 @@
-import { AppProps } from 'next/app'
+import { useEffect } from 'react'
+import type { AppProps } from 'next/app'
+
 import 'sanitize.css'
 import 'sanitize.css/forms.css'
 import 'tailwindcss/tailwind.css'
 
-import { Layout } from '../src/foundations/Layout'
-import '../src/styles/global.scss'
+import { Layout } from '@foundations/Layout'
+import '@styles/global.scss'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+	useEffect(() => {
+		document.body.classList?.remove('loading')
+	}, [])
+
 	return (
 		<Layout>
 			<Component {...pageProps} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,8 @@
 import { NextPage } from 'next'
-import Head from 'next/head'
 
 const Home: NextPage = () => {
 	return (
 		<>
-			<Head>
-				<title>socket-chat</title>
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
 			<div>asdf</div>
 		</>
 	)
